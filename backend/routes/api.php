3275ca542a998a848af16ad7bb73fb473ae179f5
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-12 22:06:56 +0300

    added trip and driver routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/login',[\App\Http\Controllers\LoginController::class,'submit']);
+Route::post('/login',[LoginController::class,'submit']);
+Route::post('/login/verify',[LoginController::class,'verify']);
+Route::group([
+    'middleware'=>'auth:sanctum'
+], function(){
+
+    Route::get('/driver',[DriverController::class,'show']);
+    Route::post('/driver',[DriverController::class,'update']);
+
+    Route::post('trip',[TripController::class,'store']);
+    Route::get('/trip/{trip}',[TripController::class,'show']);
+
+    Route::post('/trip/{trip}/accept',[TripController::class,'accept']);
+    Route::post('/trip/{trip}/start',[TripController::class,'start']);
+    Route::post('/trip/{trip}/end',[TripController::class,'end']);
+    Route::post('/trip/{trip}/location',[TripController::class,'location']);
+    Route::get('/users', function (Request $request) {
+        return $request->user();
+
+    });
+});
