cb7742064e79e44a982792c9b3b98026c80fd4c4
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-13 23:16:49 +0300

    added Trip stages

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -39,4 +39,52 @@ class TripController extends Controller
              return response()->json(['message'=>'cannot find this trip'],404);
     }
 
+    public function accept(Request $request,Trip $trip)
+    {
+         //a driver accepts a trip
+        $request->validate([
+            'driver_location'=>'required'
+        ]);
+        $trip->update([
+            'driver_id'=>$request->user()->id,
+            'driver_location'=>$request->driver_location,
+        ]);
+        $trip->load('driver.user');
+
+        return $trip;
+    }
+    public function start(Request $request,Trip $trip)
+    {
+        //a driver has started taking a passenger to their destination
+         $trip->update([
+             'is_started'=>true
+         ]);
+        $trip->load('driver.user');
+
+        return $trip;
+
+    }
+    public function end(Request $request,Trip $trip)
+    {
+        //a driver has ended a trip
+        $trip->update([
+            'is_complete'=>true
+        ]);
+        $trip->load('driver.user');
+
+        return $trip;
+
+    }
+    public function location(Request $request,Trip $trip)
+    {
+        $request->validate([
+            'driver_location'=>'required'
+        ]);
+        $trip->update([
+            'driver_location'=>$request->driver_location
+        ]);
+        return $trip;
+
+    }
+
 }
