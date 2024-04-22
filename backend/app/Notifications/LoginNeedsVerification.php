5d8261f0933665f14d0636520be40cc7f169ac7a
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-22 21:33:33 +0300

    preview logic

--- backend/app/Notifications/LoginNeedsVerification.php
+++ backend/app/Notifications/LoginNeedsVerification.php
@@ -6,12 +6,14 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Log;
 use NotificationChannels\Twilio\TwilioChannel;
 use NotificationChannels\Twilio\TwilioSmsMessage;
+use App\Traits\ClickSendSms;
 
 class LoginNeedsVerification extends Notification
 {
-    use Queueable;
+    use Queueable,ClickSendSms;
 
     /**
      * Create a new notification instance.
@@ -34,7 +36,7 @@ class LoginNeedsVerification extends Notification
     /**
      * Get the mail representation of the notification.
      */
-    public function toTwilio(object $notifiable): \NotificationChannels\Twilio\TwilioMessage|TwilioSmsMessage
+    public function toTwilio(object $notifiable)
     {
        try {
         $loginCode = rand(111111,999999);
@@ -42,12 +44,14 @@ class LoginNeedsVerification extends Notification
            'login_code'=>$loginCode
         ]);
 
+
          return (new TwilioSmsMessage("Your login code is $loginCode \n dont share this with anyone! "));
 
        } catch (\Exception $e) {
           Log::error("An error occured during submition ".$e);
        }
-      
+
+       return response("the message has been successfully sent",200);
     }
 
     /**
