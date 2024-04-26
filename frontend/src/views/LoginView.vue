0fb9cc5601c7d4280ee7c01f8cb0bd92f732821a
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-26 20:38:04 +0300

    login view

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,13 +1,20 @@
 <script setup>
 </script>
 
-<template>
-    <div class="pt-16">
+<template >
+    <div class="pt-16 flex items-center justify-center">
+       <div>
         <h1 class="text-3xl font-semibold mb-4 p-2">
             Enter your phone number
         </h1>
-
-
+       </div>
+       <div class="rounded-lg">
+        <form action="" method="post">
+            <input type="number" name="phone" id="phone">
+             <button type="submit" class="text-white bg-black">Continue</button>
+        </form>
+       </div>
+        
     </div>
     
 </template>
